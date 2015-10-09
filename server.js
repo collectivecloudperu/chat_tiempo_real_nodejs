413ba7788e767dab31cb72aec7b9d736f98737de
Collective Cloud Peru <collectivecloudperu@gmail.com>
2015-10-09 12:33:29 -0500

    Chat en Tiempo Real con Node JS
    Chat en Tiempo Real con Node JS y Socket IO

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
-var express = require('express'),
-      app = express(),
-      server = require('http').createServer(app),
-      io = require('socket.io').listen(server),
-      mensajes = [],
-      sockets = [];
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
-app.use( express.static(__dirname + '/public'));
+function send404(response) {
+    response.writeHead(404, {"Content-type" : "text/plain"});
+    response.write("Error 404: resource not found");
+    response.end();
+}
 
-server.listen(4000);
+function sendPage(response, filePath, fileContents) {
+    response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
 
-io.sockets.on('connection', function (socket) {
+function serverWorking(response, absPath) {
+    fs.exists(absPath, function(exists) {
+        if (exists) {
+            fs.readFile(absPath, function(err, data) {
+                if (err) {
+                    send404(response)
+                } else {
+                    sendPage(response, absPath, data);
+                }
+            });
+        } else {
+            send404(response);
+        }
+    });
+}
+        
+var server = http.createServer(function(request, response) {
+    var filePath = false;
 
-  sockets.push(socket);
+    if (request.url == "/") {
+        filePath = "public/index.html";
+    } else {
+        filePath = "public" + request.url;
+    }
 
-  socket.emit('messages-available', mensajes);
-
-    socket.on('add-message', function (data) {
-      mensajes.push(data);
-      sockets.forEach(function (socket) {
-        socket.emit('message-added', data);
-      });
-   });
+    var absPath = "./" + filePath;
+    serverWorking(response, absPath);
 });
+
+var port_number = server.listen(process.env.PORT || 4000);
